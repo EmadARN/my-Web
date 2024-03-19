9fe6e634420cd3dd8a26ccac0b968a94cc0ba4fa
emad <arabanemad@gmail.com>
2024-03-19 15:14:12 +0330

    initial commit

---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { VerticalTimeline } from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+import "react-vertical-timeline-component/style.min.css";
+import ExperienceCard from "./widgets/ExperienceCard";
+import { styles } from "../../styles";
+import { experiences } from "../../constants";
+import { textVariant } from "../../utils/motion";
+import SectionWrapper from "../../hoc/SectionWrapper";
+
+const Experience = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>
+          What I have done so far
+        </p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>
+          Work Experience.
+        </h2>
+      </motion.div>
+
+      <div className="mt-20 flex flex-col">
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard
+              key={`experience-${index}`}
+              experience={experience}
+            />
+          ))}
+        </VerticalTimeline>
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Experience, "work");
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import {
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
+  StarsCanvas,
+} from "./canvas";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import About from "../components/about/About.jsx";
+import Tech from "./Tech";
+import Experience from "../components/experience/Experience";
+import Works from "./Works";
+import Feedbacks from "./Feedbacks";
+import Contact from "./Contact";
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
+  StarsCanvas,
+};
